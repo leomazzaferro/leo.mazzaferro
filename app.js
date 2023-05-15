28a5e47c9cce2a62ae318204d780989af5b3039b
leomazzaferro <leonel.mazzaferro@gmail.com>
2023-05-15 02:01:22 -0300

    post put delete

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/productos", async (req, res) => {
       });
     }
   } catch (err) {
-    res.status(500).json({
+    return res.status(500).json({
       status: "Error",
       msj: err.message,
     });
@@ -40,27 +40,103 @@ app.get("/productos/:pid", async (req, res) => {
     const pid = req.params.pid;
     const product = await productManager.getProductById(pid);
     if (product) {
-      res.status(200).json({
+      return res.status(200).json({
         status: "succes",
         msj: "Producto encontrado",
         data: product,
       });
-    } else {
-      res.status(404).json({
+    } /* else {
+      return res.status(404).json({
         status: "Error",
         msj: err.message,
       });
-    }
+    }*/
   } catch (err) {
-    res.status(500).json({
+    return res.status(404).json({
       status: "Error",
       msj: err.message,
     });
   }
 });
 
+app.delete("/productos/:pid", async (req, res) => {
+  try {
+    const pid = req.params.pid;
+    await productManager.deleteProduct(pid);
+    return res.status(200).json({
+      status: "Succes.",
+      msj: "Producto eliminado.",
+      data: {},
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: "Error",
+      msj: err.message,
+    });
+  }
+});
+
+app.post("/productos", async (req, res) => {
+  try {
+    const { title, description, category, price, thumbnail, code, stock } =
+      req.body;
+    const newProduct = await productManager.addProduct(
+      title,
+      description,
+      category,
+      price,
+      thumbnail,
+      code,
+      stock
+    );
+    return res.status(201).json({
+      status: "Succes",
+      msg: "Producto agregado.",
+      data: newProduct,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: "Error",
+      msg: err.message,
+    });
+  }
+});
+
+app.put("/productos/:pid", async (req, res) => {
+  try {
+    const pid = req.params.pid;
+    const {
+      status,
+      title,
+      description,
+      category,
+      price,
+      thumbnail,
+      code,
+      stock,
+    } = req.body;
+    const productUpdate = await productManager.updateProduct(pid);
+    return res.status(200).json({
+      status: "succes",
+      msg: "Producto actualizado",
+      data: productUpdate,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: "Error",
+      msg: err.message,
+    });
+  }
+});
+
+app.get("*", (req, res) => {
+  return res.status(404).json({
+    error: "No se encuentra la ruta",
+  });
+});
+
 app.listen(PORT, () => {
-  console.log(`Servidor iniciado en puerto ${PORT}`);
+  console.log(`Servidor iniciado en puerto http://localhost:${PORT}`);
 });
 
 /*app.get("/bienvenida", (req, res) => {
--- productManager.js
+++ productManager.js
@@ -1,11 +1,12 @@
-import fs from "fs";
+import fs, { stat } from "fs";
+import { v4 as uuidv4 } from "uuid";
 
 export default class ProductManager {
   constructor() {
     //this.products = [];
     const productString = fs.readFileSync("products.json", "utf-8");
-    const product = JSON.parse(productString);
-    this.products = product;
+    const products = JSON.parse(productString);
+    this.products = products;
   }
 
   getProducts() {
@@ -21,6 +22,54 @@ export default class ProductManager {
       throw new Error("Producto no encontrado.");
     }
   }
+
+  deleteProduct(pid) {
+    let index = this.products.findIndex((product) => product.pid == pid);
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      return;
+    } else {
+      throw new Error("Producto no encontrado para borrar.");
+    }
+  }
+
+  addProduct(title, description, category, price, thumbnail, code, stock) {
+    let generateId = uuidv4();
+    let repeatCode = this.products.find((product) => product.code === code)
+      ? true
+      : false;
+    const newProduct = {
+      pid: generateId,
+      createdAt: Date.now(),
+      status: true,
+      title,
+      description,
+      category,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+    if (!title || !description || !category || !price || !code || !stock) {
+      throw new Error("Todos los campos son obligatorios.");
+    } else if (repeatCode === true) {
+      throw new Error("Codigo de producto ya ingresado.");
+    } else {
+      this.products.push(newProduct);
+    }
+  }
+
+  updateProduct(
+    pid,
+    status,
+    title,
+    description,
+    category,
+    price,
+    thumbnail,
+    code,
+    stock
+  ) {}
 }
 
 //const productManager = new ProductManager()
